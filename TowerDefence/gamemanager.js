5af6ead224175be785099809364cc0758c45cc8c
Henry Lee <nynxzx@gmail.com>
2020-11-05 23:34:31 +1000

    First Layer System Pass

--- TowerDefence/gamemanager.js
+++ TowerDefence/gamemanager.js
@@ -1,84 +1,107 @@
 class GameManager {
 
-    static Layers = {
-        GroundFloor: 0, //Inactive - We use Images
-        GroundFloorExtras: 1, 
-        GroundFloorInteractables: 2, 
-        HouseFloor: 3, 
-        HouseWalls: 4,
-        HouseInteractables: 5,
-        EnemySprites: 6,
-        PlayerCharacters: 7,
-        PlayerTools: 8,
-        Effects: 9,
-        Master: 10
-    }
+    
     
     //Array of all Players Active
     static allPlayers = [];
 
     static activePlayer = null;
     static gamePaused = false;
+    
+    //TODO MOVE?
     static SpriteGroupPaused;
-
     static debugHUD;
 
+    //#region LAYERS
+    static Layers = {};
+    static initLayers(){
+
+        GameManager.Layers.GroundFloor = createGraphics(1440, 816);
+        GameManager.Layers.GroundFloor.isEnabled = true;
+
+        //GameManager.Layers.GroundFloorExtras
+        //GameManager.Layers.GroundFloorInteractables
+        //GameManager.Layers.HouseFloor
+        //GameManager.Layers.HouseWalls
+        //GameManager.Layers.HouseInteractables
+        //GameManager.Layers.EnemySprites
+
+        GameManager.Layers.PlayerCharactersGroup = new Group();
+        GameManager.Layers.PlayerCharactersGroup.isEnabled = true;
+
+        //GameManager.Layers.PlayerTools
+
+        GameManager.Layers.Effects = createGraphics(1440, 816);
+        GameManager.Layers.Effects.isEnabled = true;
+
+        //GameManager.Layers.Master = createGraphics(1440, 816);
+        //GameManager.Layers.Master.isEnabled = true;
+
+    }
+    static drawActiveLayers(){
+
+        if(GameManager.Layers.GroundFloor.isEnabled){
+            image(GameManager.Layers.GroundFloor, 0, 0);
+        }
+        
+        //GameManager.Layers.GroundFloorExtras
+        //GameManager.Layers.GroundFloorInteractables
+        //GameManager.Layers.HouseFloor
+        //GameManager.Layers.HouseWalls
+        //GameManager.Layers.HouseInteractables
+        //GameManager.Layers.EnemySprites
+
+        if(GameManager.Layers.PlayerCharactersGroup.isEnabled){
+            GameManager.Layers.PlayerCharactersGroup.draw();
+        }
+        
+        //GameManager.Layers.PlayerTools
+
+        if(GameManager.Layers.Effects.isEnabled){
+            image(GameManager.Layers.Effects, 0, 0);
+        }
+
+        //if(GameManager.Layers.Master.isEnabled){
+            //image(GameManager.Layers.Master, 0, 0);
+        //}
+        
+        //drawSprites();
+
+    }
+    //#endregion
+
+    //#region GAME HOOKS
     static initGame(){
+        GameManager.initLayers()
 
         GameManager.SpriteGroupPaused = new Group();
-
-        // for(let i = 0; i < 3; i++ ){
-        //     for(let y = 0; y < 3; y++){
-        //         let player = new Player("Player", random(50,950), random(50,950))
-        //         GameManager.allPlayers.push(player);
-        //     }
-        // }
-
         GameManager.debugHUD = new HUD(0,0,300,height);
-        // //Make an Instance of a Player - DEBUG 100 100
-        // let debugPlayer1 = new Player("DEBUG1", 200, 400);
-
-        // //Perfomance Test
-        // for(let i = 0; i < 1000; i+=50 ){
-        //     for(let y = 0; y < 1000; y+=50){
-        //         let player = new Player("Player", i, y)
-        //         GameManager.allPlayers.push(player);
-        //     }
-        // }
-        
-        //Make an Instance of a Player - DEBUG 100 100
-        //let debugPlayer1 = new Player("DEBUG1", 200, 400);
-
-
-        //let debugHealthBar = new HealthBar();
-
-        //Push that Instance to allPlayers.
-        //GameManager.allPlayers.push(debugPlayer1);
-
 
         Map.generateMap();
 
         DebugHelpers.toggleButtons();
-        
     }
 
-    static refresh() {
-        GameManager.debugHUD.drawActivePlayerHeader();
+    static refreshGame() {
+
 
         if(Tile.activeTile){
             Tile.activeTile.markActive();
         }
         
         GameManager.allPlayers.forEach(player => {
-            player.drawInfo()
+            player.drawInfo();
             player.debugMovement();
             if(player.isSelected)
                 player._selected();
         });
 
+        //TODO: MOVE TO CONTROLS
         if(keyDown(17) && mouseWentUp(LEFT) && !GameManager.gamePaused && mouseX < width && mouseY < height && mouseX > 0 && mouseX > 0){
-            Map.getTileAtWorldPosition(mouseX, mouseY)
-            console.log(Tile.activeTile)
+            //TODO: Implement a "getSprite" or "getActor" or something, stop using onMousePressed for selection of players
+            Map.getTileAtWorldPosition(mouseX, mouseY);
+            
+            console.log(Tile.activeTile);
             if(GameManager.activePlayer){
                 GameManager.activePlayer.sprite.position.x = mouseX;
                 GameManager.activePlayer.sprite.position.y = mouseY;
@@ -87,14 +110,13 @@ class GameManager {
             //let fire = createSprite(mouseX, mouseY-25);
             //fire.addAnimation('fire', Images.Effects.Fire2);
             //fire.scale = 1;
-
         }
 
         if(keyWentDown("esc")){
             if(GameManager.activePlayer){
                 GameManager.activePlayer.isSelected = false;
                 GameManager.activePlayer = null;
-            }else {
+            } else {
                 if(!GameManager.gamePaused){ 
                     GameManager.SpriteGroupPaused.removeSprites();
                     GameManager.gamePaused = true;
@@ -104,7 +126,6 @@ class GameManager {
                     GameManager.SpriteGroupPaused.removeSprites();
                 }
             }
-            
             console.log("ESC PRESSED");
         }
 
@@ -113,6 +134,7 @@ class GameManager {
            // let spend = new Interactable(Images.Interactables.Debug.SpendTouching, mouseX, mouseY, Interactable.Type.Spend, Interactable.Range.Touching);
         }
     }
+    //#endregion
 }
     
 

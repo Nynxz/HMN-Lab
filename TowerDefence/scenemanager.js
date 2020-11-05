33a2dfa37041ba470d094b671037f4e9302e0815
Henry Lee <nynxzx@gmail.com>
2020-11-05 23:32:03 +1000

    Removed GrassDebug

--- TowerDefence/scenemanager.js
+++ TowerDefence/scenemanager.js
@@ -3,26 +3,13 @@ class SceneManager{
     static Scenes = {MainMenu: 'mainmenu', OptionsMenu: 'optionsmenu', CreditMenu: 'creditmenu', InGame: 'ingame'}
     static CurrentScene = SceneManager.Scenes.InGame;
 
+    //THIS IS FUCKED
     static debugNext(){
         let button = createSprite(width/2, height/2);
-        button.onMousePressed = function(){
+        button.onMousePressed = function() {
             allSprites.clear();
             SceneManager.CurrentScene = SceneManager.Scenes.InGame;
             GameManager.initializeGame();
         }
     }
 }
-
-class GrassDebug {
-    constructor() {
-        this.length = length;
-        this.width = width;
-    }
-    debugCreateBackground() {
-        let debugBackground = createSprite(0,0);
-        debugBackground.addImage("/TowerDefence/assets/debug/GrassDebug.png")
-        console.log(debugBackground);
-    }
-
-}
-

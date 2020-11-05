ba7fa82caf209b5545cbd767316d90216e8d6db0
Henry Lee <nynxzx@gmail.com>
2020-11-05 23:32:56 +1000

    Cleanup

--- TowerDefence/map.js
+++ TowerDefence/map.js
@@ -1,127 +1,90 @@
 class Tile{
+
+    //we use this for id's, increased every time a Tile is constructed
     static tileCount = 0;
+    //TODO: MOVE THIS TO A LAYER?
     static floorTiles = [];
     static activeTile;
 
     constructor(_rand, _x, _y, _size){
+        //Assign an id to a tile
         this.id = Tile.tileCount++;
         
+        //TODO: MOVE TO FUNCTION?
         if(_rand > .2){
             this.type = 'grass';
-            Map.MapCanvas.image(Images.Map.GrassRegular, (_x+1) * Map.tileSize - (Map.tileSize),  (_y+1) * Map.tileSize - (Map.tileSize), Map.tileSize, Map.tileSize);
+            GameManager.Layers.GroundFloor.image(Images.Map.GrassRegular, (_x+1) * Map.tileSize - (Map.tileSize),  (_y+1) * Map.tileSize - (Map.tileSize), Map.tileSize, Map.tileSize);
             this.passable = true;
-            
         } else {
-            Map.MapCanvas.image(Images.Map.GrassFlower, (_x+1) * Map.tileSize - (Map.tileSize),  (_y+1) * Map.tileSize - (Map.tileSize), Map.tileSize, Map.tileSize);
+            GameManager.Layers.GroundFloor.image(Images.Map.GrassFlower, (_x+1) * Map.tileSize - (Map.tileSize),  (_y+1) * Map.tileSize - (Map.tileSize), Map.tileSize, Map.tileSize);
             this.type = 'flower'
             this.passable = false;
         }
 
         this.pos = {x: _x, y: _y};
         
-        
-        //console.log(this.id)
-        if(_x % 91 == 0){
+        //TODO UNHARDCODE THIS
+        if(_x % 1440/16 == 0){
             Tile.floorTiles.push([]);
         }
         Tile.floorTiles[_y].push(this);
+
     }
 
-    markActive(){
-        rectMode(CORNER)
-        noFill();
-        stroke('red');
-        strokeWeight(1);
-        rect(this.pos.x * Map.tileSize, this.pos.y * Map.tileSize, Map.tileSize);
+    //We use this to mark the selected squares as active
+    markActive() {
+        GameManager.Layers.Effects.rectMode(CORNER)
+        GameManager.Layers.Effects.noFill();
+        GameManager.Layers.Effects.stroke('red');
+        GameManager.Layers.Effects.strokeWeight(1);
+        GameManager.Layers.Effects.rect(this.pos.x * Map.tileSize, this.pos.y * Map.tileSize, Map.tileSize);
     }
 }
 
 
 
 class Map{
-
+    //TODO: UNHARDCODE THIS
     static mapWidth =  1440/16;
     static mapHeight = 816/16;
 
+    //Size of the grid in pixels
     static tileSize = 16;
 
-    static randomValues = [];
-    static MapGet;
-
-
-    //Test
-    static MapCanvas;
+    //We call this once at the start of the game to generate the map
+    //We put other generation methods in here
     static generateMap(){
-        Map.MapCanvas = createGraphics(1440, 816);
-
-        Map._generateFloorTiles();
-
+        Map._generateFloorTiles();  
     }
 
-    static drawFloorTiles(){
-
-        image(Map.MapCanvas, 0, 0);
-
-        // //We Generate Random Values, Once to Save
-        // for(let x=0; x < Map.mapWidth; x++){
-        //     for(let y=0; y < Map.mapHeight; y++){
-        //         if(Map.randomValues[x][y] > .2){
-        //             image(Images.Map.GrassRegular, (x+1) * Map.tileSize - (Map.tileSize),  (y+1) * Map.tileSize - (Map.tileSize), Map.tileSize, Map.tileSize);
-        //         } else {
-        //             image(Images.Map.GrassFlower, (x+1) * Map.tileSize - (Map.tileSize),  (y+1) * Map.tileSize - (Map.tileSize), Map.tileSize, Map.tileSize);
-        //         }
-        //     }
-        // }
-        
-    }
-
-    static async logTileInfo(){
-        // let bool = true;
-        // console.log("LOGGING START");
-
-        // setTimeout(() =>{
-        //     console.log("LOGGING OFF");
-        //     bool = false;
-        // }, 500);
-        // while(bool){
-        //     console.log("LOGGING ON");
-        // }
-    }
-
+    //We call this to return a Tile or  TODO > Tile's
+    //TODO: Add swap if multiple tiles?
     static getTileAtWorldPosition(_x, _y){
-        Map.logTileInfo()
+
+        //Magic? :eyes: - grabs the tile from the 2d array 
+        //TODO: Add some security checks maybe.. 
         let tile = Tile.floorTiles[floor(_y / Map.tileSize)][floor( _x / Map.tileSize)];
-        Tile.activeTile = tile;
-        return Tile.floorTiles[floor(_y / Map.tileSize)][floor( _x / Map.tileSize)]
+
+        //Toggle Tile on and off
+        if(Tile.activeTile == tile){
+            Tile.activeTile = null
+        } else {
+            Tile.activeTile = tile;
+        }
     }
 
+    //We call this when we want to regenerate the randomness of the floor tiles
     static _generateFloorTiles(){
+
+        //Reset the arrays
         Tile.floorTiles = [];
-        Map.randomValues = [];
-        for(let x=0; x < Map.mapWidth; x++){
-            Map.randomValues.push([]);
+
+        //Magic stuff
+        for(let x=0; x < Map.mapWidth; x++){;
             for(let y=0; y < Map.mapHeight; y++){
                 let rand = random(0,1)
-                Map.randomValues[x].push(rand); 
                 new Tile(rand, x, y);            
             }
         }
-
-        console.log(Tile.floorTiles);
-
-         //We Generate Random Values, Once to Save
-        //  for(let x=0; x < Map.mapWidth; x++){
-        //     for(let y=0; y < Map.mapHeight; y++){
-        //         if(Map.randomValues[x][y] > .2){
-        //             Map.MapCanvas.image(Images.Map.GrassRegular, (x+1) * Map.tileSize - (Map.tileSize),  (y+1) * Map.tileSize - (Map.tileSize), Map.tileSize, Map.tileSize);
-        //         } else {
-        //             Map.MapCanvas.image(Images.Map.GrassFlower, (x+1) * Map.tileSize - (Map.tileSize),  (y+1) * Map.tileSize - (Map.tileSize), Map.tileSize, Map.tileSize);
-        //         }
-        //     }
-        // }
-
-        console.log("DONE");
     }
-
-
 }
